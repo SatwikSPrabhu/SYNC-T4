ef8cea607d5cef9b5a45ed2c94e8dcdea6e83b0d
SatwikSPrabhu <117463364+SatwikSPrabhu@users.noreply.github.com>
2023-07-25 21:24:22 +0530

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,20 @@
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+function w3_close() {
+    mySidebar.style.display = "none";
+}
